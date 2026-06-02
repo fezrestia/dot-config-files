2bfb662d36c44d1e187899104c7e283e93328d1f
Aki.SHIMO <fezrestia@gmail.com>
2026-06-02 11:16:30 +0900

    ADD: initial .pythonrc.py

---
+++ .pythonrc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Enable .pythonrc.py by following.
+#
+# $ export PYTHONSTARTUP=~/.pythonrc.py
+#
+
+
+# Standard Modules
+import os
+import sys
+
+import pprint
+pp = pprint.pprint
+
+try:
+    import numpy as np
+except ImportError:
+    pass
+
